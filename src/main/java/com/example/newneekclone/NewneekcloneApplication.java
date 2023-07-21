1d5dd1d535bef6d9eb467905eb60df8251f07093
Eulga <kimyeoun10@gmail.com>
2023-07-21 15:45:48 +0900

    init: init commit

---
+++ src/main/java/com/example/newneekclone/NewneekcloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.newneekclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewneekcloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewneekcloneApplication.class, args);
+    }
+
+}
